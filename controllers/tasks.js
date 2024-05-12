a1cdc6e5a25d2c685a525742767c7146f0c8aa78
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-12 18:17:39 +0300

    delete task controller refactor

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -35,8 +35,17 @@ const updateTask = (req, res) => {
   res.send("update task");
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
