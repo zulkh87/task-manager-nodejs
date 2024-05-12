8639562a632389ffd40680b8b2b511df70b5b4e8
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-12 18:34:18 +0300

    update task controller refactor

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -31,8 +31,20 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("update task");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({});
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const deleteTask = async (req, res) => {
